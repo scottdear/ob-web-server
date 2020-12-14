d4541cc8effba1682c5b2d2b8fe118d44e5f5637
Ata Mahmoud <ata.ata250@hotmail.com>
2020-12-14 19:10:49 +0200

    add cors support

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const winston = require('winston');
+const cors=require('cors');
+
+app.use(cors());
 require('dotenv').config();
 
 require('./startup/logging')();
